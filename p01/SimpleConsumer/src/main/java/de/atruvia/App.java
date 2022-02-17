2b6bcc72f1e607458715a73a2abd5e2f39162b01
Jo Wagner <jowagner@limago.de>
2022-02-17 14:30:51 +0100

    Initial commit

---
+++ p01/SimpleConsumer/src/main/java/de/atruvia/App.java
@@ -0,0 +1,15 @@
+package de.atruvia;
+
+import atruvia.SimpleKafkaConsumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        new SimpleKafkaConsumer().run();
+    }
+}
