5294037bc37567fb95d5284db1de0111f15f94e4
Jo Wagner <jowagner@limago.de>
2022-02-18 14:31:36 +0100

    InitialCommit

---
+++ p01/SimpleSpringJSONConsumer/src/main/java/de/atruvia/simplespringjsonconsumer/SimpleSpringJsonConsumerApplication.java
@@ -0,0 +1,13 @@
+package de.atruvia.simplespringjsonconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringJsonConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringJsonConsumerApplication.class, args);
+    }
+
+}
